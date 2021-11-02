15e546d6c6e893461a05143a08b692c5a15e2629
Rupali Gupta <rupali-gupta@hotmail.com>
2021-11-02 14:29:02 -0600

    First commit

---
+++ src/components/portfolio/Portfolio.js
@@ -0,0 +1,11 @@
+import "./portfolio.scss"
+
+function Portfolio() {
+    return (
+        <div className= "portfolio" id = "portfolio">
+            Hello Portfolio
+        </div>
+    )
+}
+
+export default Portfolio
