10bdd703fd74259c47f88e214977f71af0c17345
Rupali Gupta <rupali-gupta@hotmail.com>
2021-11-03 11:05:15 -0600

    added images in assets and completed design for intro page

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,9 +1,40 @@
 import "./intro.scss"
+import { init } from 'ityped'
+import { useEffect,useRef } from "react"
 
 function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, { 
+            showCursor: true, 
+            backDelay: 1500,
+            backSpeed: 60,
+            strings: [' Web', ' Development' ] 
+        });
+    }, [])
     return (
         <div className="intro" id = "intro">
-            Hello Intro
+            <div className="left">
+                <div className="imageContainer">
+                    <img src="assets/man.png" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hello There, I'm</h2>
+                    <h1>PCL Forge</h1>
+                    <h3>technical  
+                        <span ref = {textRef}>
+                            
+                        </span>
+                        </h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt="" />
+                </a>
+            </div>
         </div>
     )
 }
